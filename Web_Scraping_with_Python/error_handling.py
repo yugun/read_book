d5517563626b8b707b19f88cd699cc5a61cb08fc
Yugun Kang <yugun819@pumpkinnet.com>
2019-10-06 22:35:48 +0900

    add first error handling

---
+++ Web_Scraping_with_Python/error_handling.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+try:
+	#html = urlopen('http://www.pythonscraping.com/pages/error.html')
+	html = urlopen('http://www.pythonscrapingthisurldoesnotexist.com')
+except HTTPError as e:
+	print(e)
+except URLError as e:
+	print('The server could not be found!')
+else:
+	print('It Worked!')
+
