0a0484dcad2c4e827b3689b4dab42f0a91271ad3
Yugun Kang <yugun819@pumpkinnet.com>
2019-10-06 23:08:40 +0900

    find prince!

--- Web_Scraping_with_Python/beautiful_find.py
+++ Web_Scraping_with_Python/beautiful_find.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 html = urlopen('http://www.pythonscraping.com/pages/warandpeace.html')
 bs = BeautifulSoup(html, 'html.parser')
 
-nameList = bs.findAll('span', {'class': 'green'})
+nameList = bs.findAll(text = 'the prince')
+print(len(nameList))
 for name in nameList:
-	print(name.get_text())
+	print(name)
