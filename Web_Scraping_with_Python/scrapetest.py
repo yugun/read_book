355e27cf6cb8f505f45c1f54db230a0bcae02a0b
Yugun Kang <yugun819@pumpkinnet.com>
2019-10-06 22:25:00 +0900

    add BeautifulSoup

--- Web_Scraping_with_Python/scrapetest.py
+++ Web_Scraping_with_Python/scrapetest.py
@@ -1,4 +1,10 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 html = urlopen('http://pythonscraping.com/pages/page1.html')
-print(html.read())
+bs = BeautifulSoup(html.read(), 'html.parser')
+
+print(bs.html.body.h1)
+print(bs.body.h1)
+print(bs.html.h1)
+print(bs.h1)
